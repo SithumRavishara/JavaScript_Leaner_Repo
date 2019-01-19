aaf3863f5822bdfc4f69b7038cae730237ecf632
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2019-01-19 23:52:50 +0530

    What is Scope in JS? added file

---
+++ scope.js
@@ -0,0 +1,24 @@
+/*let imGlobal = 'Outer'
+
+if (true) {
+    let imLocal = 'Inner'
+    console.log(imGlobal);
+    console.log(imLocal);
+}
+
+console.log(imGlobal);*/
+//console.log(imLocal);
+
+//let king = 'john'
+
+if (true) {
+    //let king ='wick'
+    if (true) {
+         king = 'rawana'
+        console.log("I am part one"+king)
+    }
+}
+
+if (true) {
+    console.log("I am Second part"+king)
+}
