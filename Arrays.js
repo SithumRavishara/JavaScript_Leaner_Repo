ea219cff0b85ba104c4f6697ff51dade54f593e0
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2019-01-20 22:18:36 +0530

    Array file

---
+++ Arrays.js
@@ -0,0 +1,12 @@
+// Arrays
+
+const friends = ['Yohan','Bathiya','Ossa','Huiya']
+
+console.log(friends)
+console.log(friends[0])
+console.log(friends[3])
+console.log(friends.length)
+console.log(friends[friends.length-1])
+
+
+console.log(`We have ${friends[friends.length-1]} super guys.`)
