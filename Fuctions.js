e617b1ce6708b9caf2286fb51f5bca498a0debe0
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2019-01-20 22:20:02 +0530

    Function file

---
+++ Fuctions.js
@@ -0,0 +1,22 @@
+
+
+let method1 = function(name){
+console.log("say! hello to new "+ name+'!')
+console.log(`say ${name}!`)
+}
+
+method1('buff')
+
+let fullname = function(name1,name2){
+console.log(`Hello Mr. ${name1} and ${name2}`)
+
+}
+
+fullname('sithum','ravishara')
+
+let sum = function(num1,num2,num3){
+    let add = num1+num2+num3
+    return add;
+}
+
+console.log('Addition:-'+sum(2,3,5))
