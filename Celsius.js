269a7d02cabc875e5acf579a045a44236ba39a50
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2019-01-20 22:18:18 +0530

    Constant file

---
+++ Celsius.js
@@ -0,0 +1,14 @@
+var score = 100
+
+score = 130
+
+const bonus = 50
+
+var Finaltotal = score+bonus*1.5
+console.log(Finaltotal)
+
+let celsius = 100
+
+let faranhyd = (celsius-32)*5/9
+
+console.log(faranhyd)
