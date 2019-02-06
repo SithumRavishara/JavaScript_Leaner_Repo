9018d0384409eb2c2f8b2712b9dbf27b22c51b40
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2019-02-06 23:02:22 +0530

    file added

---
+++ Arrayfunctions.js
@@ -0,0 +1,32 @@
+ const numbers = ['one','two','three','four','five','six']
+
+// numbers[1] = 'something'
+// console.log(numbers)
+
+
+
+ //start
+numbers.shift()
+console.log(numbers);
+
+console.log(numbers.shift());
+
+numbers.unshift('something')
+console.log(numbers);
+
+ //End
+console.log(numbers.pop());
+
+console.log(numbers);
+
+numbers.push('johuuu')
+console.log(numbers);
+
+console.log(numbers.push("jo"));
+
+
+ //middle
+
+ numbers.splice(2,2,'lowa');
+ console.log(numbers);
+ 
