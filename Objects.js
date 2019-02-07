792b42616defd0a0ff1b25f77c1f630e0e6a2b3b
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2019-02-07 23:05:03 +0530

    Create Object code

---
+++ Objects.js
@@ -0,0 +1,31 @@
+
+let myfirst = {
+   objectname : 'test',
+   task : 15,
+   content : true,
+
+}
+
+console.log(myfirst);
+
+
+let newapp = {
+    objective : "To Do List",
+    Developer : "Sithum Ravishara",
+    task : 'Palan every task in your day.'
+
+
+}
+
+console.log(`Hey this is my ${newapp.objective} app for ${newapp.task} and it's owner ${newapp.Developer}.`);
+
+
+let  mycourse = {
+    Title : 'ReactNative advance',
+    Audience : 'Indergraduates',
+    fee : 'Rs.20 000',
+    description : "This is awesome course for be genuine your skills,It's all about your strngth",
+
+}
+
+console.log(`${mycourse.Title} course for ${mycourse.Audience} and it's fee ${mycourse.fee}.${mycourse.description}`)
