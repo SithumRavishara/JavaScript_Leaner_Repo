adc79cbef5e31eac9239650d048745e6864306b5
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2019-03-15 21:59:51 +0530

    Create Equals file

---
+++ equals.js
@@ -0,0 +1,10 @@
+let one = ''
+let two = ''
+
+console.log(one==two);// == check equal asign values
+
+let cat = {}
+let dog = {}
+
+console.log(cat===dog); // objects are refer memory in speratly so objects are differ each to another
+
