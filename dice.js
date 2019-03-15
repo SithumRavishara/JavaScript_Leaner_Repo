57b42f2a976bb80ecb94b605d0474186e5d7f0a5
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2019-03-15 21:59:18 +0530

    Added file

---
+++ dice.js
@@ -0,0 +1,20 @@
+num1 = 17.0
+num2 = 7
+
+result = 17/7
+ console.log(result);
+ 
+ console.log(result.toFixed(2));
+
+ let floatres = result.toFixed(2)
+
+//  console.log(Math.floor(floatres));
+//  console.log(Math.ceil(floatres));
+ 
+ let upper = 6
+ let lowwer = 1
+
+ let myRandom =Math.floor(Math.random()* (upper - lowwer + 1) + lowwer)
+ 
+ console.log(myRandom);
+ 
